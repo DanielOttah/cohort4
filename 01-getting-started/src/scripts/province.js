b36c51f16154838e779aa1aa8fc1579429bc38e8
Daniel Ottah <daniel.ottah@live.co.uk>
2020-03-06 13:44:15 -0700

    refactoring

--- 01-getting-started/src/scripts/province.js
+++ 01-getting-started/src/scripts/province.js
@@ -1,19 +1,17 @@
 const provinceCode = {
     getProvince: (pC) => {
-
-        if (pC == "AB" || pC == "Ab" || pC == "aB" || pC == "ab") return "Alberta";
-        else if (pC == "BC" || pC == "Bc" || pC == "bC" || pC == "bc") return "British Columbia";
-        else if (pC == "MB" || pC == "Mb" || pC == "mB" || pC == "mb") return "Manitoba";
-        else if (pC == "NB" || pC == "Nb" || pC == "nB" || pC == "nb") return "New Brunswick";
-        else if (pC == "NL" || pC == "Nl" || pC == "nL" || pC == "nl") return "Newfoundland and Labrador";
-        else if (pC == "NS" || pC == "Ns" || pC == "nS" || pC == "ns") return "Nova Scotia";
-        else if (pC == "ON" || pC == "On" || pC == "oN" || pC == "on") return "Ontario";
-        else if (pC == "PE" || pC == "Pe" || pC == "pE" || pC == "pe") return "Prince Edward Island";
-        else if (pC == "SK" || pC == "Sk" || pC == "sK" || pC == "sk") return "Saskatchewan";
-        else if (pC == "NT" || pC == "Nt" || pC == "nT" || pC == "nt") return "Northwest Territories";
-        else if (pC == "NU" || pC == "Nu" || pC == "nU" || pC == "nu") return "Nunavut";
-        else if (pC == "YT" || pC == "Yt" || pC == "yT" || pC == "yt") return "Yukon";
-        else if (pC == "QC" || pC == "Qc" || pC == "qC" || pC == "qc") return "Quebec";
+        if (pC == "AB" || pC == "aB" || pC == "Ab" || pC == "ab") return "Alberta";
+        else if (pC == "BC" || pC == "bC" || pC == "Bc" || pC == "bc") { return "British Columbia"; } else if (pC == "MB" || pC == "mB" || pC == "Mb" || pC == "mb") return "Manitoba";
+        else if (pC == "NB" || pC == "NB" || pC == "NB" || pC == "nb") return "New Brunswick";
+        else if (pC == "NL" || pC == "nL" || pC == "Nl" || pC == "nl") return "Newfoundland and Labrador";
+        else if (pC == "NS" || pC == "nS" || pC == "Ns" || pC == "ns") return "Nova Scotia";
+        else if (pC == "ON" || pC == "oN" || pC == "On" || pC == "on") return "Ontario";
+        else if (pC == "PE" || pC == "pE" || pC == "Pe" || pC == "pe") return "Prince Edward Island";
+        else if (pC == "SK" || pC == "sK" || pC == "Sk" || pC == "sk") return "Saskatchewan";
+        else if (pC == "NT" || pC == "nT" || pC == "Nt" || pC == "nt") return "Northwest Territories";
+        else if (pC == "NU" || pC == "nU" || pC == "Nu" || pC == "nu") return "Nunavut";
+        else if (pC == "YT" || pC == "yT" || pC == "Yt" || pC == "yt") return "Yukon";
+        else if (pC == "QC" || pC == "qC" || pC == "Qc" || pC == "qc") return "Quebec";
         else {
             return "Not a valid provincial code";
         }
