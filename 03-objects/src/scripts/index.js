9da076c5a0c8f3277c8debe7a4b02884d4851f4e
Daniel Ottah <daniel.ottah@live.co.uk>
2020-03-23 16:55:22 -0600

    update on cities

--- 03-objects/src/scripts/index.js
+++ 03-objects/src/scripts/index.js
@@ -228,8 +228,6 @@ function addNewCity() {
         div1.className = "pullLeft";
         div1.classList.add("panelShow");
         div1.id = `infoContainer${count}`;
-        div1.style = "height:460;"
-        div1.style = "overflow-y:scroll;"
         div1.appendChild(createPElement(`City Population: ${ct.getPopulationofCity(cityName.value)}`));
         div1.appendChild(createPElement(`City Latitude: ${ct.newCt.allCities[count - 1].latitude}`));
         div1.appendChild(createPElement(`City Longitude: ${ct.newCt.allCities[count - 1].longitude}`));
@@ -267,23 +265,49 @@ function createPElement(txt) { // Creates <p> tag Elements and attaches the text
 
 function createButtonElement(txt) { // Creates <button> tag Elements and attaches the texts
     let btn = document.createElement('Button');
-    btn.appendChild(document.createTextNode(txt));
+    if (txt == "Edit City") {
+        btn.appendChild(document.createTextNode(txt));
+        btn.id = `edit${count}`;
+    } else if (txt == "Delete City") {
+        btn.appendChild(document.createTextNode(txt));
+        btn.id = `delete${count}`;
+    }
+
     return btn;
 }
 
 function getElementClicked(event) {
     let x = event.target;
-    return x.id;
+    return x;
 }
 
 function cityButtons() {
     let elId = getElementClicked(event); //get id of element
-    let num = elId.substring(7, elId.length); //get id number
-    let cont = document.getElementById(`infoContainer${num}`)
-    if (cont.style.display == "block") {
-        cont.style.display = "none";
-    } else {
-        cont.style.display = "block";
+    let num = elId.id.substring(7, elId.length); //get id number
+    if (elId.id.includes("btnCity")) {
+        let cont = document.getElementById(`infoContainer${num}`);
+        if (cont.style.display == "block") {
+            cont.style.display = "none";
+        } else {
+            cont.style.display = "block";
+            document.querySelector("#gmap_canvas1").src = `https://maps.google.com/maps?q=${(elId.textContent).toLowerCase()}&t=&z=13&ie=UTF8&iwloc=&output=embed`;
+        }
+    } else if (elId.id.includes("delete")) {
+        let cityToDelete = elId.parentNode.previousElementSibling.textContent; //get name of city to delete
+        ct.deleteCity(cityToDelete); //parse name to delete city from array store
+        elId.parentNode.previousElementSibling.remove(); //delete button 
+        elId.parentNode.remove(); //delete city info container
+        document.querySelector("#gmap_canvas1").src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d18876468.200307723!2d-113.72221585646199!3d54.72270517403909!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4b0d03d337cc6ad9%3A0x9968b72aa2438fa5!2sCanada!5e0!3m2!1sen!2sca!4v1584990289869!5m2!1sen!2sca";
+
+    } else if (elId.id.includes("edit")) {
+        console.log("seeing edit");
+
+        let ed = document.getElementById("editCityInfo");
+        if (ed.style.display == "block") {
+            ed.style.display = "none";
+        } else {
+            ed.style.display = "block";
+        }
     }
 
 }
